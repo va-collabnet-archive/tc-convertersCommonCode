34f4decb99af8064f0821369166d7d37c6b61070
darmbrust <daniel.armbrust.list@gmail.com>
2013-06-17 22:29:32 +0000

    making a release

--- src/main/java/gov/va/oia/terminology/converters/sharedUtils/UuidFromName.java
+++ src/main/java/gov/va/oia/terminology/converters/sharedUtils/UuidFromName.java
@@ -15,7 +15,10 @@ public class UuidFromName
 	 */
 	public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException
 	{
-		System.out.println(Type5UuidFactory.get(Type5UuidFactory.PATH_ID_FROM_FS_DESC, "NDFRT Path"));
+		System.out.println(Type5UuidFactory.get(Type5UuidFactory.PATH_ID_FROM_FS_DESC, "VA JIF Terminology Workbench development path"));
+		System.out.println(Type5UuidFactory.get(Type5UuidFactory.PATH_ID_FROM_FS_DESC, "VA JIF Terminology Workbench development origin"));
+		System.out.println(Type5UuidFactory.get(Type5UuidFactory.PATH_ID_FROM_FS_DESC, "VA JIF Terminology Workbench release candidate path"));
+		System.out.println(Type5UuidFactory.get(Type5UuidFactory.PATH_ID_FROM_FS_DESC, "VA JIF maintained module (core metadata concept)"));
 
 	}
 
