8c654b1e0e216c230bb0da9b3e21b94ec14af992
darmbrust <daniel.armbrust.list@gmail.com>
2013-06-27 06:28:00 +0000

    formatting cleanup

--- src/main/java/gov/va/oia/terminology/converters/sharedUtils/stats/LoadStats.java
+++ src/main/java/gov/va/oia/terminology/converters/sharedUtils/stats/LoadStats.java
@@ -6,7 +6,7 @@ import java.util.TreeMap;
 
 /**
  * Keep counts on all of the types of things that are converted.
- *  
+ * 
  * @author Daniel Armbrust
  */
 
@@ -24,48 +24,48 @@ public class LoadStats
 	{
 		concepts_++;
 	}
-	
+
 	public int getConceptCount()
 	{
 		return concepts_;
 	}
-	
+
 	public void addDescription(String descName)
 	{
 		increment(descriptions_, descName);
 	}
-	
+
 	public void addConceptId(String idName)
 	{
 		increment(conceptIds_, idName);
 	}
-	
+
 	public void addComponentId(String annotatedItem, String annotationName)
-    {
-        increment(componentIds_, annotatedItem, annotationName);
-    }
-	
+	{
+		increment(componentIds_, annotatedItem, annotationName);
+	}
+
 	public void addAnnotation(String annotatedItem, String annotationName)
 	{
 		increment(annotations_, annotatedItem, annotationName);
 	}
-	
+
 	public void addRefsetMember(String refsetName)
 	{
 		increment(refsetMembers_, refsetName);
 	}
-	
+
 	public void addRelationship(String relName)
 	{
 		increment(relationships_, relName);
 	}
-	
+
 	public ArrayList<String> getSummary()
 	{
 		ArrayList<String> result = new ArrayList<String>();
-		
+
 		result.add("Concepts: " + concepts_);
-		
+
 		int sum = 0;
 		for (Map.Entry<String, Integer> value : relationships_.entrySet())
 		{
@@ -73,7 +73,7 @@ public class LoadStats
 			result.add("Relationship '" + value.getKey() + "': " + value.getValue());
 		}
 		result.add("Relationships Total: " + sum);
-		
+
 		sum = 0;
 		for (Map.Entry<String, Integer> value : conceptIds_.entrySet())
 		{
@@ -81,25 +81,25 @@ public class LoadStats
 			result.add("Concept ID '" + value.getKey() + "': " + value.getValue());
 		}
 		result.add("Concept IDs Total: " + sum);
-		
+
 		sum = 0;
-        int nestedSum = 0;
-        for (Map.Entry<String, TreeMap<String, Integer>> value : componentIds_.entrySet())
-        {
-            nestedSum = 0;
-            for (Map.Entry<String, Integer> nestedValue : value.getValue().entrySet())
-            {
-                result.add("Component ID '" + value.getKey() + ":" + nestedValue.getKey() + "': " + nestedValue.getValue());
-                nestedSum += nestedValue.getValue();
-            }
-            sum += nestedSum;
-            if (value.getValue().size() > 1)
-            {
-                result.add("Component ID '" + value.getKey() + "' Total: " + nestedSum);
-            }
-        }
-        result.add("Component IDs Total: " + sum);
-		
+		int nestedSum = 0;
+		for (Map.Entry<String, TreeMap<String, Integer>> value : componentIds_.entrySet())
+		{
+			nestedSum = 0;
+			for (Map.Entry<String, Integer> nestedValue : value.getValue().entrySet())
+			{
+				result.add("Component ID '" + value.getKey() + ":" + nestedValue.getKey() + "': " + nestedValue.getValue());
+				nestedSum += nestedValue.getValue();
+			}
+			sum += nestedSum;
+			if (value.getValue().size() > 1)
+			{
+				result.add("Component ID '" + value.getKey() + "' Total: " + nestedSum);
+			}
+		}
+		result.add("Component IDs Total: " + sum);
+
 		sum = 0;
 		for (Map.Entry<String, Integer> value : descriptions_.entrySet())
 		{
@@ -107,7 +107,7 @@ public class LoadStats
 			result.add("Description '" + value.getKey() + "': " + value.getValue());
 		}
 		result.add("Descriptions Total: " + sum);
-		
+
 		sum = 0;
 		nestedSum = 0;
 		for (Map.Entry<String, TreeMap<String, Integer>> value : annotations_.entrySet())
@@ -125,7 +125,7 @@ public class LoadStats
 			}
 		}
 		result.add("Annotations Total: " + sum);
-		
+
 		sum = 0;
 		for (Map.Entry<String, Integer> value : refsetMembers_.entrySet())
 		{
@@ -133,11 +133,10 @@ public class LoadStats
 			result.add("Refset Members '" + value.getKey() + "': " + value.getValue());
 		}
 		result.add("Refset Members Total: " + sum);
-		
-		
+
 		return result;
 	}
-	
+
 	private void increment(TreeMap<String, Integer> dataHolder, String type)
 	{
 		Integer i = dataHolder.get(type);
@@ -151,18 +150,18 @@ public class LoadStats
 		}
 		dataHolder.put(type, i);
 	}
-	
+
 	private void increment(TreeMap<String, TreeMap<String, Integer>> dataHolder, String annotatedType, String type)
 	{
 		TreeMap<String, Integer> map = dataHolder.get(annotatedType);
-		
+
 		if (map == null)
 		{
 			map = new TreeMap<String, Integer>();
 		}
-		
+
 		Integer i = map.get(type);
-				
+
 		if (i == null)
 		{
 			i = new Integer(1);
